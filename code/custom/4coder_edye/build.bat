b7635d64259187b37be3c7b82360dfb939b27f55
Ed Ye <edwardye@protonmail.com>
2024-07-02 17:14:13 +0800

    building personal layer on windows

--- code/custom/4coder_edye/build.bat
+++ code/custom/4coder_edye/build.bat
@@ -1,4 +1,7 @@
 @echo off
-call ..\bin\buildsuper_x64-win.bat .\4coder_fleury.cpp release
-copy .\custom_4coder.dll ..\..\custom_4coder.dll
-copy .\custom_4coder.pdb ..\..\custom_4coder.pdb
+call ..\bin\buildsuper_x64-win.bat .\4coder_edye.cpp debug 
+::release
+::move ..\..\..\build\custom_4coder.dll ..\..\..\build\custom_4coder.old.dll
+::move ..\..\..\build\custom_4coder.pdb ..\..\..\build\custom_4coder.old.pdb
+::copy .\custom_4coder.dll ..\..\..\build\custom_4coder.dll
+::copy .\custom_4coder.pdb ..\..\..\build\custom_4coder.pdb
