342bd23382bb58a6e25b0be61d1cd16697a6c417
Tim Apple <tim@MacBook-Pro.local>
2024-08-09 21:06:27 +0900

    stuck on org mode

--- code/custom/4coder_edye/4coder_edye.cpp
+++ code/custom/4coder_edye/4coder_edye.cpp
@@ -2363,7 +2363,8 @@ edye_register_languages(void){
         }
     }*/
     
-    // NOTE(edye): org mode
+    // TODO(edye): org mode, can't seem to plug in a simple lexer
+    /*
     {
         F4_RegisterLanguage(S8Lit("org"),
                             edye_org_IndexFile,
@@ -2373,6 +2374,7 @@ edye_register_languages(void){
                             edye_org_Highlight,
                             Lex_State_Cpp);
     }
+    */
 }
 
 void custom_layer_init(Application_Links *app)
