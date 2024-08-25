70d90ce8c606304e70292e4192bfcafde84b7f27
Tim Apple <tim@MacBook-Pro.local>
2024-08-25 21:50:26 +0900

    mac build script

--- build-mac.sh
+++ build-mac.sh
@@ -10,7 +10,7 @@ SRC_ROOT="$LOCATION/code"
 PROJECT_ROOT="$(dirname "$SRC_ROOT")" # relative path to root
 
 if [ ! -d "$PROJECT_ROOT/build" ]; then
-	mkdir "$PROJECT_ROOT/build"
+mkdir "$PROJECT_ROOT/build"
 fi
 
 BUILD_ROOT="$PROJECT_ROOT/build"
@@ -20,6 +20,8 @@ CUSTOM_BIN="$CUSTOM_ROOT/bin"
 
 echo "---"
 echo "Building Executable ..."
-sh "$BIN_ROOT/build-mac.sh" "-DDEV_BUILD" # "-DPACKAGE_SUPER_X64"
+
+# DEV_BUILD has profiling prints out each frame, which are annoying
+sh "$BIN_ROOT/build-mac.sh" "-DPACKAGE_SUPER_X64" # "-DDEV_BUILD" 
 
 sh "$PROJECT_ROOT/build-custom-mac.sh"
