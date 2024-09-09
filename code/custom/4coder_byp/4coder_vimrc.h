8f21848bb20c54b8fb0f74f4b90a7731dc01ec19
Ed Ye <edwardye@protonmail.com>
2024-09-10 02:08:31 +0900

    lister num columns

--- code/custom/4coder_byp/4coder_vimrc.h
+++ code/custom/4coder_byp/4coder_vimrc.h
@@ -7,7 +7,7 @@ enum BYP_Vim_Request{
 	BYP_REQUEST_Title,
 	BYP_REQUEST_Comment,
 	BYP_REQUEST_UnComment,
-
+    
 	BYP_REQUEST_COUNT
 };
 
@@ -16,7 +16,7 @@ enum BYP_Vim_Objects{
 	BYP_OBJECT_param1,
 	BYP_OBJECT_camel0,
 	BYP_OBJECT_camel1,
-
+    
 	BYP_OBJECT_COUNT
 };
 #define VIM_ADDITIONAL_REQUESTS        BYP_REQUEST_COUNT
@@ -27,5 +27,5 @@ enum BYP_Vim_Objects{
 #define VIM_DEFAULT_REGISTER           unnamed
 
 #define VIM_USE_BOTTOM_LISTER          1
-#define VIM_LISTER_RANGE               1,3
+#define VIM_LISTER_RANGE               3,3
 #define VIM_LISTER_MAX_RATIO           0.35f
