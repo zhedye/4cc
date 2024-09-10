f0193b184dd9101daa38ea46ec0352ee8210fe4e
Ed Ye <edwardye@protonmail.com>
2024-09-11 02:57:07 +0900

    fix windows build.sh

--- build.sh
+++ build.sh
@@ -17,30 +17,34 @@ if [[  ! -v gcc && ! -v tcc ]]; then clang=1; fi
 if [ ! -v super ];      then debug=1; fi
 if [ -v debug ];        then echo "[dev_build]"; flag="-DDEV_BUILD"; fi
 if [ -v super ];        then echo "[package_super_x64]"; flag="-DPACKAGE_SUPER_X64"; fi
-if [ -v custom_layer ]; then customer_layer=1; echo "[custom layer only]"; fi
+if [ -v custom_layer_only ]; then custom_layer_only=1; echo "[custom layer only]"; fi
 
 # NOTE: mac DEV_BUILD has profiling prints out each frame, which are annoying
 
 # --- Compile/Link Line Definitions -------------------------------------------
-os="$(uname -s)" # operating system name
-# TODO do i even need exec_cmd? idk. test without it on windows git bash
-if [ $os == "Windows" ]; then exec_cmd="call"; script="build.bat"; custom_layer_script="buildsuper_x64-win.bat"; fi
-if [ $os == "Linux" ];   then exec_cmd="bash"; script="build-linux.sh"; custom_layer_script="buildsuper_x64-linux.sh"; fi
+os="$(uname -o)" # operating system name
+# TODO(edye): is exec_cmd necessary?
+# msys = windows git bash
+if [ $os == "Msys" ]; then script="build.bat"; custom_layer_script="buildsuper_x64-win.bat"; custom_layer_name="custom_4coder.dll"; fi
+if [ $os == "Linux" ];   then script="build-linux.sh"; custom_layer_script="buildsuper_x64-linux.sh"; custom_layer_name="custom_4coder.so";  fi
 
 # Mac OS
-if [ $os == "Darwin" ];  then exec_cmd="bash"; script="build-mac.sh"; custom_layer_script="buildsuper_x64-mac.sh"; fi
+if [ $os == "Darwin" ];  then script="build-mac.sh"; custom_layer_script="buildsuper_x64-mac.sh"; custom_layer_name="custom_4coder.so";  fi
 
-if [ ! -v custom_layer ]; then
-build_cmd=("$exec_cmd" "code/bin/$script" "$flag")
-"${build_cmd[@]}"
+if [ ! -v custom_layer_only ]; then
+    pushd "code"
+    build_cmd=("bin/$script" "$flag")
+    "${build_cmd[@]}"
+    popd
 fi
 
+### --- Custom Layer ----------------------------------------------------------
 pushd "code/custom/4coder_edye"
 
-build_custom_cmd=("$exec_cmd" "../bin/$custom_layer_script" "4coder_edye.cpp")
+build_custom_cmd=("../bin/$custom_layer_script" "4coder_edye.cpp")
 "${build_custom_cmd[@]}"
 
-cp "custom_4coder.so" "../../../build/custom_4coder.so"
+cp "$custom_layer_name" "../../../build/$custom_layer_name"
 
 # link my bindings
 if [ ! -f "../../../build/bindings.4coder" ]; then
